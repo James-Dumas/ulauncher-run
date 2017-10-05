df163ba7ad853309fa358839b33af8eb40d651bc
James-Dumas <jamesdumas418@gmail.com>
2017-10-04 23:44:43 -0400

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+A [ulaucher](https://ulauncher.io/) extension to run shell commands
+
+type `run` followed by a command
+
---
+++ main.py
@@ -0,0 +1,24 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+import subprocess
+
+class RunExtension(Extension):
+
+    def __init__(self):
+        super(DemoExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        command = event.query.split(" ", 1)[1]
+        subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+if __name__ == '__main__':
+    RunExtension().run()
+
