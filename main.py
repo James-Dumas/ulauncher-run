b68cf0de83ca33f4d4722889f8d00a337bfd56af
jamac <jamac@localhost.localdomain>
2018-01-28 17:45:53 -0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ulauncher.api.client.Extension import Extension
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
@@ -16,17 +17,25 @@ class RunExtension(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        command = event.query.split(" ", 1)[1]
+        data = event.get_argument()
         items = [
                 ExtensionResultItem(
                     icon="images/icon.png",
                     name="Run a shell command",
-                    description="Run '%s'" % command,
-                    on_enter=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT),
+                    description="Run '%s'" % data,
+                    on_enter=ExtensionCustomAction(data),
                     ),
                 ]
 
         return RenderResultListAction(items)
+
+class ItemEnterEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        data = event.get_data()
+        subprocess.Popen(data.split(" "), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        return RenderResultListAction([])
 
 if __name__ == '__main__':
     RunExtension().run()
