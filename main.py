98500f322b124656793b31bf348c6e1e2c7552da
James Dumas <jamesdumas418@gmail.com>
2018-01-29 16:21:58 -0500

    fixed typo

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-import subprocessm, shlex
+import subprocess
+import shlex
 
 class RunExtension(Extension):
 
